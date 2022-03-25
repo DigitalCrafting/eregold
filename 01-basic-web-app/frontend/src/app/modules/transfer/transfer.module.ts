bfd4574611b626d76eef7e4ce82e913153956836
DigitalCrafting <k.kasprzyk2@gmail.com>
2022-03-25 13:44:33 +0100

    01 - FE - Created transfer component

---
+++ 01-basic-web-app/frontend/src/app/modules/transfer/transfer.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TransferComponent} from './components/transfer.component';
+
+@NgModule({
+    declarations: [
+        TransferComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+export class TransferModule {
+}
