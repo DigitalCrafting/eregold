95b1ebab94db401df708eccef41900cc3aa79962
DigitalCrafting <k.kasprzyk2@gmail.com>
2022-05-01 11:31:28 +0200

    03 - MW - Implemented CustomersController

--- 03-web-app-plus-monolith-middleware/middleware/api/src/main/java/org/digitalcrafting/arkenstone/api/customers/CustomersController.java
+++ 03-web-app-plus-monolith-middleware/middleware/api/src/main/java/org/digitalcrafting/arkenstone/api/customers/CustomersController.java
@@ -1,12 +1,28 @@
 package org.digitalcrafting.arkenstone.api.customers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.digitalcrafting.arkenstone.domain.customers.CustomerDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/customers")
 public class CustomersController {
     private final CustomersControllerService service;
+
+    @GetMapping
+    public CustomerDTO getById(@RequestParam String customerId) {
+        return service.getById(customerId);
+    }
+
+    @GetMapping
+    public CustomerDTO getByEmail(@RequestParam String email) {
+        return service.getByEmail(email);
+    }
+
+    @PostMapping
+    public void createCustomer(@RequestBody CustomerDTO customerDTO) {
+        service.createCustomer(customerDTO);
+    }
+
 }
--- 03-web-app-plus-monolith-middleware/middleware/api/src/main/java/org/digitalcrafting/arkenstone/api/customers/CustomersControllerService.java
+++ 03-web-app-plus-monolith-middleware/middleware/api/src/main/java/org/digitalcrafting/arkenstone/api/customers/CustomersControllerService.java
@@ -1,7 +1,29 @@
 package org.digitalcrafting.arkenstone.api.customers;
 
+import lombok.RequiredArgsConstructor;
+import org.digitalcrafting.arkenstone.domain.customers.CustomerDTO;
+import org.digitalcrafting.arkenstone.domain.customers.CustomersConverter;
+import org.digitalcrafting.arkenstone.repository.customers.CustomerEntity;
+import org.digitalcrafting.arkenstone.repository.customers.CustomersEntityManager;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomersControllerService {
+    private final CustomersEntityManager entityManager;
+
+    public CustomerDTO getById(String customerId) {
+        CustomerEntity entity = entityManager.getByEmail(customerId);
+        return CustomersConverter.toDTO(entity);
+    }
+
+    public CustomerDTO getByEmail(String email) {
+        CustomerEntity entity = entityManager.getByEmail(email);
+        return CustomersConverter.toDTO(entity);
+    }
+
+    public void createCustomer(CustomerDTO customerDTO) {
+        CustomerEntity entity = CustomersConverter.toEntity(customerDTO);
+        entityManager.insert(entity);
+    }
 }
